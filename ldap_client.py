3eeb61ac31c7f983879d9cdaa7160c22a9e7f3a0
Larry Samuels <larry_samuels@hotmal.com>
2023-07-13 21:04:36 +0000

    Change function in ldap_client to accept parameters New main in ldap_client to send parameters to functions

--- ldap_client.py
+++ ldap_client.py
@@ -13,20 +13,17 @@ set_library_log_activation_level(logging.DEBUG)
 from ldap3.utils.log import set_library_log_detail_level, OFF, BASIC, NETWORK, EXTENDED
 set_library_log_detail_level(BASIC)
 
-PASSWORD='rahasak'
-connection = None
 
-def connect_ldap_server():
+def connect_ldap_server(server_url, auth_dn, auth_password):
     global connection
     try:
         
         # Provide the hostname and port number of the openLDAP      
-        server_uri = f"ldap://localhost:1389"
-        server = Server(server_uri, get_info=ALL)
+        server = Server(server_url, get_info=ALL)
         # username and password can be configured during openldap setup
         connection = Connection(server,          
-                                user='cn=admin,dc=rahasak,dc=com', 
-                                password=PASSWORD)
+                                user=auth_dn, 
+                                password=auth_password)
         print(connection)
         bind_response = connection.bind() # Returns True or False 
         print(bind_response)
@@ -35,22 +32,22 @@ def connect_ldap_server():
         connection = e
 
 # For groups provide a groupid number instead of a uidNumber
-def get_ldap_users():
+def get_ldap_users(server_url, auth_dn, auth_password, search_base, search_filter, search_scope):
     
     # Provide a search base to search for.
-    search_base = 'dc=rahasak,dc=com'
+    # search_base = 'dc=rahasak,dc=com'
     # provide a uidNumber to search for. '*" to fetch all users/groups
     #search_filter = '(uidNumber=500)'
     #search_filter = '(cn=*)'
-    search_filter = '(objectClass=*)'
+    #search_filter = '(objectClass=*)'
 
     # Establish connection to the server
-    ldap_conn = connect_ldap_server()
+    ldap_conn = connect_ldap_server(server_url, auth_dn, auth_password)
     try:
         # only the attributes specified will be returned
         ldap_conn.search(search_base=search_base,       
                          search_filter=search_filter,
-                         search_scope=SUBTREE, 
+                         search_scope=search_scope, 
                          #attributes=['cn','sn','uid','uidNumber'])
                          attributes=['*'])
         # search will not return any values.
@@ -60,26 +57,10 @@ def get_ldap_users():
     except LDAPException as e:
         results = e
         
-def add_ldap_user():
-# sample attributes 
-    ldap_attr = {
-        "cn": "Bill Doe",
-        "gecos": "Do user",
-        "gidNumber": '23405',
-        "homeDirectory": "/home/jdoe",
-        "loginShell": "/bin/bash",
-        "mail": "jdoe@rahasak.com",
-        "sn": '6',
-        "uid": "jdoe",
-        "uidNumber": '21355',
-        "userPassword": b'(SSHA)asafasf'
-    }
+def add_ldap_user(server_url, auth_dn, auth_password, user_dn, ldap_attr):
 
     # Bind connection to LDAP server
-    ldap_conn = connect_ldap_server()
-
-    # this will create testuser
-    user_dn = "sn=6,dc=rahasak,dc=com"
+    ldap_conn = connect_ldap_server(server_url, auth_dn, auth_password)
 
     try:
         # object class for a user is inetOrgPerson
@@ -95,38 +76,30 @@ def add_ldap_user():
     print(response)
     return response   
 
-def modify_ldap_user(): 
+def modify_ldap_user(server_url, auth_dn, auth_password, modify_user_dn, changes, controls): 
     
     # Bind connection to LDAP server
-    ldap_conn = connect_ldap_server()
-    
-    # this will create testuser
-    user_dn = "sn=6,dc=rahasak,dc=com"
-    
+    ldap_conn = connect_ldap_server(server_url, auth_dn, auth_password)
+        
     try:
         # object class for a user is inetOrgPerson
         # perform the Modify operation
-        response = ldap_conn.modify(user_dn,
-                         {'cn': [(MODIFY_REPLACE, ['cn-1-replaced'])],
-                          'gecos': [(MODIFY_REPLACE, ['gecos-replaced'])]})
+        response = ldap_conn.modify(modify_user_dn,changes,controls)
         
     except LDAPException as e:
         response = e
     print(response)
     return response   
 
-def delete_user(): 
+def delete_user(server_url, auth_dn, auth_password, delete_user_dn): 
     
     # Bind connection to LDAP server
-    ldap_conn = connect_ldap_server()
-    
-    # this will create testuser
-    user_dn = "sn=5,dc=rahasak,dc=com"
-    
+    ldap_conn = connect_ldap_server(server_url, auth_dn, auth_password)
+        
     try:
         # object class for a user is inetOrgPerson
         # perform the Modify operation
-        response = ldap_conn.delete(user_dn)
+        response = ldap_conn.delete(delete_user_dn)
         
     except LDAPException as e:
         response = e
@@ -135,8 +108,51 @@ def delete_user():
 
 
 if __name__ == '__main__':
-    get_ldap_users()
-    #add_ldap_user()
-    #modify_ldap_user()
-    #delete_user()
+    # Needed for connect_ldap_server
+    server_url = f"ldap://localhost:1389"
+    auth_dn = 'cn=admin,dc=rahasak,dc=com'
+    auth_password = 'rahasak'
+
+
+    # Needed for get_ldap_users
+    search_base = 'dc=rahasak,dc=com'
+    # provide a uidNumber to search for. '*" to fetch all users/groups
+    #search_filter = '(uidNumber=500)'
+    #search_filter = '(cn=*)'
+    search_filter = '(objectClass=*)'
+    search_scope = SUBTREE
+    get_ldap_users(server_url, auth_dn, auth_password, search_base, search_filter, search_scope)
+    #----------------------------------------------------------------------#
+    
+    # for add_ldap_user
+    #attributes 
+    ldap_attr = {
+        "cn": "Jane Edwards",
+        "gecos": "JE user",
+        "gidNumber": '23406',
+        "homeDirectory": "/home/jedw",
+        "loginShell": "/bin/bash",
+        "mail": "jedw@rahasak.com",
+        "sn": '7',
+        "uid": "jedw",
+        "uidNumber": '21356',
+        "userPassword": b'(SSHA)aspfasf'
+    }
+    user_dn = "sn=7,dc=rahasak,dc=com"
+    # add_ldap_user(server_url, auth_dn, auth_password, user_dn, ldap_attr)
+    #----------------------------------------------------------------------#
+    
+    # for modify ldap user
+    modify_user_dn = "sn=6,dc=rahasak,dc=com"
+    changes = {
+        'cn': [(MODIFY_REPLACE, ['cn-3-replaced'])],
+        'gecos': [(MODIFY_REPLACE, ['gecos-3-replaced'])]
+      }
+    controls = None
+    #modify_ldap_user(server_url, auth_dn, auth_password, modify_user_dn, changes, controls)
+    #----------------------------------------------------------------------#
+    
+    # For delete_user
+    delete_user_dn = "sn=7,dc=rahasak,dc=com"
+    # delete_user(server_url, auth_dn, auth_password, delete_user_dn)
     
