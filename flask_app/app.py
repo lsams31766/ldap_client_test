2137b2de0c9cb63425a5ff2a09304fe8cd00a65b
Robert Jaeger <jaegerr1@bms.com>
2023-09-08 15:15:58 +0000

    updates to flask app

--- flask_app/app.py
+++ flask_app/app.py
@@ -5,22 +5,33 @@ sys.path.append('../')
 from ldap_client import *
 from ldap3 import MODIFY_REPLACE, SUBTREE
 app = Flask(__name__)
+import json
+import base64
+
+# temp to encode passwords not used in final app
+# sample_string = "r2d2" 
+# sample_string_bytes = sample_string.encode("ascii")
+# base64_bytes = base64.b64encode(sample_string_bytes)
+# base64_string = base64_bytes.decode("ascii")  
+# print(f"Encoded string: {base64_string}")
+
+#Load server info at startup
+# Opening JSON files
+f = open('servers.json')
+server_data = json.load(f)
+f.close()
+f = open('creds.json')
+local_creds_data = json.load(f)
+f.close()
+
 
 ### LDAP processing routines
-prod_dirs = [
-  {'name':'access management','creds':'edir01','search_base':'o=bms.com'},
-  {'name':'enterprise directory ','creds':'enterprise','search_base':'o=bms.com'},
-  {'name':'meta-supplier','creds':'meta-supplier','search_base':'o=bms.com'},
-  {'name':'one','creds':'adjoin-na','search_base':'DC=one,DC=ads,DC=bms,DC=com'},
-  {'name':'forge rock unified','creds':'frds','search_base':'o=bms.com'},
-  {'name':'metaview','creds':'metaview','search_base':'o=bms.com'}  
-]
 
-def get_prod_dir_for_name(dir_name, env_selected):
+def get_prod_uat_dir_for_name(dir_name, env_selected):
     if env_selected == 'PROD':
-        cur_dirs = prod_dirs
+        cur_dirs = server_data["prod_dirs"]
     elif env_selected == 'UAT':
-        cur_dirs = uat_dirs    
+        cur_dirs = server_data["uat_dirs"]    
     else:
         print('BAD ENVIRONMENT selected')
         return    
@@ -29,40 +40,37 @@ def get_prod_dir_for_name(dir_name, env_selected):
             return d
     return None
 
-other_prod_dirs = [
-  {'name':'celcorp','creds':'uslvlspadsadc01','search_base':'DC=celgene,DC=com'},
-]
-
-uat_dirs = [
-  {'name':'access management','creds':'smlxpolhr81','search_base':'o=bms.com'},
-  {'name':'enterprise directory','creds':'enterprise-uat','search_base':'o=bms.com'},
-  {'name':'meta-supplier','creds':'metaqa-supplier','search_base':'o=bms.com'},
-  {'name':'uno','creds':'uno','search_base':'DC=one,DC=ads,DC=bms,DC=com'},
-  {'name':'forge rock qa','creds':'dsusr-1-r1-qa','search_base':'o=bms.com'},
-  {'name':'metaview-uat','creds':'metaview-uat','search_base':'o=bms.com'},
-]
+def format_server_name(name,url):
+    # clean the display of name an url - assuming longest name is 18 chars
+    s = name.ljust(22,'.') + url
+    return s
 
 dir_servers_chosen = []
-def get_dir_servers_selected(env_selected):
-    # assuming we are doing prod lookups
+def get_dir_servers_selected(env_selected): 
+
     servers = []
     if env_selected == 'PROD':
-        cur_dirs = prod_dirs
+        cur_dirs = server_data["prod_dirs"]
     elif env_selected == 'UAT':
-        cur_dirs = uat_dirs    
+        cur_dirs = server_data["uat_dirs"]   
     else:
         print('BAD ENVIRONMENT selected')
         return
     all_checked = True
     for item in cur_dirs:
+        d = {'name':item['name'],'checked':False, 
+            "display_name":format_server_name(item['name'],item['url'])}
         if item['name'] in dir_servers_chosen:
-            servers.append({'name':item['name'],'checked':True})
+            d['checked'] = True
         else:
-            servers.append({'name':item['name'],'checked':False})
             all_checked = False
+        servers.append(d)
     # add the all button, determine if it should be checked
-    servers.append({'name':'ALL/NONE','checked':all_checked})
+    servers.append({'name':'ALL/NONE','checked':all_checked,"display_name":"ALL/NONE"})
+    #print(f'-->get_dirs_selected {servers}')
     return servers
+
+
 
 def clean_entry(token):
     # eitherh line: 'mail'
@@ -81,7 +89,7 @@ def sort_output(d_results, attrs):
     if type(d_results) == dict:
         return d_results # nothing to sort
     out_list = []
-    print(f'++attrs {attrs}')
+    #print(f'++attrs {attrs}')
     for attr in attrs:
         if d_results:
             local_list = list(filter(lambda d: attr in d, d_results)) #get items that have attr as a key
@@ -101,11 +109,12 @@ def sort_output(d_results, attrs):
         out_list.extend(d_results) # add any left over items
     return out_list
 
-def format_output(server, results, attrs):
+def format_output(server, results, attrs, env_selected):
     # txt_out += s + ': ' + str(result_to_dict(results)) + '\n
     d_results = result_to_dict(results)
     if type(d_results) == dict:
-        out_str = server + ':' + '\n'
+        details = get_server_details(server, env_selected)
+        out_str = details + ':' + '\n'
         for k,v in d_results.items():
             out_str += '   ' + clean_entry(k) + ': ' + clean_entry(v)
         out_str += '\n'
@@ -119,6 +128,50 @@ def format_output(server, results, attrs):
             if a in item:
                 out_str += '   ' + clean_entry(a) + ': ' + clean_entry(item[a])
         out_str += '\n'
+    return out_str
+
+def decode_base64(base64_string):
+    base64_bytes = base64_string.encode("ascii")
+    sample_string_bytes = base64.b64decode(base64_bytes)
+    return sample_string_bytes.decode("ascii")
+
+def get_local_creds(server_name,environment):
+    # get credentials from local_creds_data - in memory verion of creds.json
+    # list format: [host, pw, user, port]
+    creds = []
+    if environment == 'PROD':
+        data = local_creds_data['prod_creds']
+    else:
+        data = local_creds_data['uat_creds']
+    for item in data:
+        print(f'get_local_creds item {item}')
+        if item['name'] == server_name:
+            creds = [item['host'], decode_base64(item['pw']), item['user'],item['port']]
+            return creds
+    print(f'ERROR get_local_creds did not find creds for {server_name}!')
+    return None # not found
+
+def get_server_details(s,env_selected):
+    #server_data['uat_dirs'] or server_data['prod_dirs']
+    # print(f'get_server_details {s} {env_selected}')
+    if env_selected == 'PROD':
+        data = server_data['prod_dirs']
+    else:
+        data = server_data['uat_dirs']
+    # {"name":"Grasc","creds":"radiusdir01","search_base":"o=bms.com",
+    #  "url":"radiusdir01.aws.bms.com","description":"Remote Access (Radius)",
+    #  "je":"Grasc"},
+    out_str = ''
+    for item in data:
+        # print(f'compare {item} to {s}')
+        if item['name'] == s:
+            # print(f'compare {item["name"]} to {s}')
+            if item['description']:
+                out_str += f"{item['description']} "
+            if item['je']:
+                out_str += f" - je connector: {item['je']}"
+            return out_str
+    # not found
     return out_str
 
 def process_query(servers_selected, new_attributes, new_filter,env_selected):
@@ -153,19 +206,26 @@ def process_query(servers_selected, new_attributes, new_filter,env_selected):
         if s == 'ALL/NONE':
             continue
         # print(f'-->{s}<--')
-        d = get_prod_dir_for_name(s,env_selected)
+        d = get_prod_uat_dir_for_name(s,env_selected)
         if not d or len(d.keys()) == 0:
             print('NO CREDS FOR ',s)
             return
-        dir_creds = get_creds_from_server(d['creds'])
-        # print(f'-->dir_creds {dir_creds}')
+        if d['creds'] == 'local': # use creds.json file
+            dir_creds = get_local_creds(s,env_selected)
+        else:
+            dir_creds = get_creds_from_server(d['creds'])
+        #print(f'-->dir_creds {dir_creds}')
         # print(f'==>new_filter {new_filter}')
-        r,results = paged_search(dir_creds, d['search_base'], 
-        new_filter, search_scope, attr_list)
+        try:
+            r,results = paged_search(dir_creds, d['search_base'], 
+            new_filter, search_scope, attr_list)
+        except:
+            r = False
+            results = []
         # for now do raw output - TODO format this output
-        #print(f'search results {results} r {r}')
+        # print(f'search results {results} r {r}')
         #txt_out += s + ': ' + str(result_to_dict(results)) + '\n'\
-        txt_out += format_output(s, results, attr_list)
+        txt_out += format_output(s, results, attr_list, env_selected)
     #print(f'==>{txt_out}<==')
     return txt_out
 
@@ -182,12 +242,13 @@ def index():
     #print(servers_selected)
     if request.method == "POST":
         d = request.form.to_dict()
-        # print(f'POST got {d}')
+        print(f'POST got {d}')
         # FORMAT: {'meta-supplier': '1', 'one': '1', 'forge rock unified': '1', 'metaview': '1', 
         # 'submit': 'Post', 'set_attributes': 'password', 'set_criteria': 'bmsid=2027'}
         # get the servers selected
         dir_servers_chosen = []
         server_names = [i['name'] for i in servers_selected]
+        #print(f'server_names {server_names}')
         for n in d.keys():
             if n in server_names:
                 dir_servers_chosen.append(n)
@@ -196,6 +257,7 @@ def index():
         new_filter = d['set_criteria']
         servers_selected = get_dir_servers_selected(env_selected)
         servers_to_search = [s['name'] for s in servers_selected if s['checked']==True]
+        #print(f'servers_selected: {servers_selected}')
         # process query only if btnSubmit clicked
         filter_output='None'
         if 'btnSubmit' in d.keys():
